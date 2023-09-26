adeb8d625c614d454494944e828106acc5699fb3
LEE SHI-WEN <TP062634@mail.apu.edu.my>
2023-09-27 00:54:58 +0800

    Setting up Identity Framework

---
+++ BedAndBreakfastGroup16/Areas/Identity/Data/BedAndBreakfastGroup16Context.cs
@@ -0,0 +1,22 @@
+﻿using BedAndBreakfastGroup16.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BedAndBreakfastGroup16.Data;
+
+public class BedAndBreakfastGroup16Context : IdentityDbContext<BedAndBreakfastGroup16User>
+{
+    public BedAndBreakfastGroup16Context(DbContextOptions<BedAndBreakfastGroup16Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- BedAndBreakfastGroup16/Program.cs
+++ BedAndBreakfastGroup16/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BedAndBreakfastGroup16.Data;
+using BedAndBreakfastGroup16.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BedAndBreakfastGroup16ContextConnection") ?? throw new InvalidOperationException("Connection string 'BedAndBreakfastGroup16ContextConnection' not found.");
+
+builder.Services.AddDbContext<BedAndBreakfastGroup16Context>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<BedAndBreakfastGroup16User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BedAndBreakfastGroup16Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
