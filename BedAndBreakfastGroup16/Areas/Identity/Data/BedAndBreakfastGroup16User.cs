8e12a76fcb277045983c7dfc0faa84f498a3b94f
LEE SHI-WEN <TP062634@mail.apu.edu.my>
2023-09-27 02:19:40 +0800

    add columns in user table and edit register page

--- BedAndBreakfastGroup16/Areas/Identity/Data/BedAndBreakfastGroup16User.cs
+++ BedAndBreakfastGroup16/Areas/Identity/Data/BedAndBreakfastGroup16User.cs
@@ -9,5 +9,16 @@ namespace BedAndBreakfastGroup16.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the BedAndBreakfastGroup16User class
 public class BedAndBreakfastGroup16User : IdentityUser
 {
+    [PersonalData]
+    public string CustomerFullName { get; set; }
+
+    [PersonalData]
+    public int CustomerAge { get; set; }
+
+    [PersonalData]
+    public string CustomerAddress { get; set; }
+
+    [PersonalData]
+    public DateTime CustomerDoB { get; set; }
 }
 
