2ea6aa43f73962e6362ef6ef1a10f6c6758048a8
peiying0921 <108811850+peiying0921@users.noreply.github.com>
2023-10-04 17:49:56 +0800

    home, login, register, about us

--- BedAndBreakfastGroup16/Controllers/HomeController.cs
+++ BedAndBreakfastGroup16/Controllers/HomeController.cs
@@ -28,5 +28,12 @@ namespace BedAndBreakfastGroup16.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+
     }
 }
