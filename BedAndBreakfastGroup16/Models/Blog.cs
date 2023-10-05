58e546d63554aa2ceec82301a1f12a8fbf679be5
peiying0921 <108811850+peiying0921@users.noreply.github.com>
2023-10-05 23:56:48 +0800

    edit home page

---
+++ BedAndBreakfastGroup16/Models/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BedAndBreakfastGroup16.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogType { get; set; }
+        public string BlogImage { get; set; }
+        public string BlogDescription { get; set; }
+    }
+}
